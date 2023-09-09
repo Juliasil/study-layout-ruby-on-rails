cfe40a8c45f0682ccd28134f9413901aa9e0ebe1
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-09-08 22:20:36 -0300

    making some adjustments to the layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+   get 'stimulus', to: 'spa#index'
    root "home#index"
 end
